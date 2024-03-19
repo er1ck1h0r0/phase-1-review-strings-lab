828e692a4b261fea40a30fd67ebd86afcf7f5049
eric <ericmaina11455@gmail.com>
2024-03-19 17:53:22 +0300

    completed-lab

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser='Peter Griffin';
+const welcomeMessage = 'Welcome to Flatbook, Peter Griffin !';
+const excitedWelcomeMessage='WELCOME TO FLATBOOK, PETER GRIFFIN!';
+const shortGreeting='Welcome, P !';
